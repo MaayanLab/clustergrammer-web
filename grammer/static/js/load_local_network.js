a9f3dd92089b2392296f9b6fee8fad70c020a6ea
cornhundred <doctorcorn2002@gmail.com>
2015-07-14 14:21:51 -0400

    visualizing example network is working, but visualization has some bugs that need to be fixed

--- grammer/static/js/load_local_network.js
+++ grammer/static/js/load_local_network.js
@@ -8,17 +8,18 @@ function change_zscore_clustergram(inst_zscore_cutoff){
 
 };
 
-function load_new_clustergram(){
+function load_new_clustergram(network){
 
-	// use d3 to load a json 
-	d3.json('/grammer/static/networks/example_network.json', function(network_data){
+	// // use d3 to load a json 
+	// d3.json('/grammer/static/networks/example_network.json', function(network_data){
 
-		// make global copy of network_data 
-		global_network_data = network_data;
+		// // make global copy of network_data 
+		// global_network_data = network_data;
+
 		// pass the network data to d3_clustergram 
-		make_d3_clustergram(network_data);
+		make_d3_clustergram(network);
 
-	});
+	// });
 
 }
 
