d7e0702630bebb737a8194862ca6c4255803d728
cornhundred <doctorcorn2002@gmail.com>
2015-07-14 16:35:11 -0400

    restricting filetype to .txt and .tsv

--- grammer/app.py
+++ grammer/app.py
@@ -30,13 +30,16 @@ SERVER_ROOT = os.path.dirname(os.getcwd()) + '/grammer/grammer' ## original
 # SERVER_ROOT = '/app/grammer'
 
 
+# define allowed extension
+ALLOWED_EXTENSIONS = set(['txt', 'tsv'])
+
+def allowed_file(filename):
+  return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
 @app.route(ENTRY_POINT + '/<path:path>') ## original 
 # @crossdomain(origin='*')
 def send_static(path):
-
-  # print('path and SERVER_ROOT')
-  # print(path)
-  # print(SERVER_ROOT)
   
   return send_from_directory(SERVER_ROOT, path)
 
@@ -45,33 +48,6 @@ def send_static(path):
 def index():
   print('Rendering index template')
   return render_template("index.html")
-
-# post request
-@app.route('/grammer/', methods=['GET','POST'])
-def python_function():
-  import flask 
-  # import make_enr_clust
-  import make_exp_clustergram
-  import json 
-  import json_scripts 
-  import sys
-  import cookielib, poster, urllib2, json
-
-  error = None 
-
-  # get request json 
-  req_json = request.form
-
-  # get the gene class 
-  inst_name = req_json['name'] # request.form['name'].upper()
-
-  # generate an expression clustergram using cst code 
-  d3_json = make_exp_clustergram.tf_clust(inst_name)
-
-  # jsonify network 
-  #####################
-  return flask.jsonify(d3_json)
-
 
 
 # Jquery upload file 
@@ -86,11 +62,15 @@ def jquery_upload_function():
   # # don't know if I need this 
   # error = None 
 
-  # python dbeugger
-  ####################
+  # # python dbeugger
+  # ###################
   # import pdb; pdb.set_trace()
+
+  # get the filename 
+  inst_filename = flask.request.files['file'].filename
   
-  if request.method == 'POST':
+  # check that request is a post
+  if request.method == 'POST' and allowed_file(inst_filename):
 
     # read file
     lines = flask.request.files['file'].readlines()
@@ -111,8 +91,6 @@ def jquery_upload_function():
 
       # strip each element 
       inst_line = [z.strip() for z in inst_line]
-
-      print(inst_line)
 
       # get column labels from first row 
       if i == 0:
@@ -164,9 +142,14 @@ def jquery_upload_function():
     # return the network in json form 
     return flask.jsonify(d3_json)
 
-  # return an error if the request is not a post 
   else:
-    return 'erorr'
+    print('error in file upload - check filetype')
+
+    return 'something'
+
+  # # return an error if the request is not a post 
+  # else:
+  #   return 'erorr'
 
 
 
