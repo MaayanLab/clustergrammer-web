15e3ca1b0e839ce56ad305d16fe8df3a893d2703
cornhundred <doctorcorn2002@gmail.com>
2015-09-16 14:43:10 -0400

    deployed improved l1000cds2 with Qianoan changes

--- clustergrammer/app.py
+++ clustergrammer/app.py
@@ -25,11 +25,11 @@ ENTRY_POINT = '/clustergrammer'
 # docker_vs_local
 ##########################################
 
-# for local development 
-SERVER_ROOT = os.path.dirname(os.getcwd()) + '/clustergrammer/clustergrammer' 
+# # for local development 
+# SERVER_ROOT = os.path.dirname(os.getcwd()) + '/clustergrammer/clustergrammer' 
 
 # for docker development
-# SERVER_ROOT = '/app/clustergrammer'
+SERVER_ROOT = '/app/clustergrammer'
 
 ######################################
 
