40c8b2c25dd485c60c6cb419380d72d168fba21d
cornhundred <doctorcorn2002@gmail.com>
2015-12-01 17:21:38 -0500

    working on demo view on homepage

--- clustergrammer/app.py
+++ clustergrammer/app.py
@@ -81,6 +81,30 @@ def viz(user_objid):
 
   return render_template('viz.html', viz_network=d3_json, viz_name=viz_name)
 
+@app.route("/clustergrammer/demo/<user_objid>")
+def demo(user_objid):
+  import flask
+  from bson.objectid import ObjectId
+  from copy import deepcopy
+
+  client = MongoClient(mongo_address)
+  db = client.clustergrammer
+
+  try: 
+    obj_id = ObjectId(user_objid)
+  except:
+    error_desc = 'Invalid visualization Id.'
+    return redirect('/clustergrammer/error/'+error_desc)
+
+  gnet = db.networks.find_one({'_id': obj_id })
+
+  client.close()
+
+  d3_json = gnet['viz']
+  viz_name = gnet['name']
+
+  return render_template('demo.html', viz_network=d3_json, viz_name=viz_name)  
+
 @app.route("/clustergrammer/enr_viz/<user_objid>")
 def enr_viz(user_objid):
   import flask
