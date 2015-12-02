a346101f44da4e938b4c9b7d982d4b3a7d5d6e3b
cornhundred <doctorcorn2002@gmail.com>
2015-12-02 11:01:03 -0500

    increased timeout and request body size in boot.sh to allow production apache server to wait longer for a response

--- boot.sh
+++ boot.sh
@@ -1,6 +1,6 @@
 adduser --disabled-password --gecos '' r
 cd /app/
-mod_wsgi-express setup-server wsgi.py --port=80 --user r --group r --server-root=/etc/app
+mod_wsgi-express setup-server wsgi.py --port=80 --user r --group r --server-root=/etc/g2e --socket-timeout=600 --limit-request-body=524288000
 chmod a+x /etc/app/handler.wsgi
 /etc/app/apachectl start
 tail -f /etc/app/error_log
