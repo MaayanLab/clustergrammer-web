1f49844d0f383b8eabf32c32d79b0141e608ab2b
cornhundred <doctorcorn2002@gmail.com>
2016-01-04 18:20:33 -0500

    deploying with N_row_sum filtering as default for vector_upload endpoint

--- clustergrammer/app.py
+++ clustergrammer/app.py
@@ -22,23 +22,23 @@ ENTRY_POINT = '/clustergrammer'
 # address for mongodbs 
 
 # # local
-mongo_address = '10.125.168.66'
+# mongo_address = '10.125.168.66'
 
-# # lab 
-# mongo_address = '146.203.54.165'
+# lab 
+mongo_address = '146.203.54.165'
 
 ##########################################
 # switch for local and docker development 
 # docker_vs_local
 ##########################################
 
-# for local development 
-SERVER_ROOT = os.path.dirname(os.getcwd()) + '/clustergrammer/clustergrammer' 
+# # for local development 
+# SERVER_ROOT = os.path.dirname(os.getcwd()) + '/clustergrammer/clustergrammer' 
 
-# # for docker development
-# SERVER_ROOT = '/app/clustergrammer'
-# # change routing of logs when running docker 
-# logging.basicConfig(stream=sys.stderr) 
+# for docker development
+SERVER_ROOT = '/app/clustergrammer'
+# change routing of logs when running docker 
+logging.basicConfig(stream=sys.stderr) 
 
 ######################################
 
