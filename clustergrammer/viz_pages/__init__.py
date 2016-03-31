436d0a0e1c30d426012dd5e5c39638c3e7bd1d54
cornhundred <doctorcorn2002@gmail.com>
2016-03-31 16:17:53 -0400

    unstable, cleaned up viz_pages blueprint, making viz_functions module to store viz_functions

--- clustergrammer/viz_pages/__init__.py
+++ clustergrammer/viz_pages/__init__.py
@@ -29,9 +29,7 @@ def render_page(net, page_route):
     error_desc = 'Invalid visualization Id.'
     return redirect('/clustergrammer/error/'+error_desc)  
 
-
 def add_routes(app=None):
-
 
   viz_page = Blueprint('viz_pages',__name__, static_url_path='/viz_pages/static', 
     static_folder='./static', template_folder='./templates')
@@ -39,75 +37,25 @@ def add_routes(app=None):
   @viz_page.route("/clustergrammer/viz/<user_objid>")
   @viz_page.route("/clustergrammer/viz/<user_objid>/<slug>")
   def viz(user_objid, slug=None):
-
     net = get_network_from_mongo(user_objid)
-
     return render_page(net, 'viz.html')
 
   @viz_page.route("/clustergrammer/Enrichr/<user_objid>")
   @viz_page.route("/clustergrammer/Enrichr/<user_objid>/<slug>")
   def viz_Enrichr(user_objid, slug=None):
-
-    client = MongoClient(mongo_address)
-    db = client.clustergrammer
-
-    try: 
-      obj_id = ObjectId(user_objid)
-    except:
-      error_desc = 'Invalid visualization Id.'
-      return redirect('/clustergrammer/error/'+error_desc)
-
-    gnet = db.networks.find_one({'_id': obj_id })
-
-    client.close()
-
-    d3_json = gnet['viz']
-    viz_name = gnet['name']
-
-    return render_template('Enrichr.html', viz_network=d3_json, viz_name=viz_name)    
+    net = get_network_from_mongo(user_objid)
+    return render_page(net, 'Enrichr.html')
 
   @viz_page.route("/clustergrammer/gen3va/<user_objid>")
   @viz_page.route("/clustergrammer/gen3va/<user_objid>/<slug>")
   def viz_gen3va(user_objid, slug=None):
-
-    client = MongoClient(mongo_address)
-    db = client.clustergrammer
-
-    try: 
-      obj_id = ObjectId(user_objid)
-    except:
-      error_desc = 'Invalid visualization Id.'
-      return redirect('/clustergrammer/error/'+error_desc)
-
-    gnet = db.networks.find_one({'_id': obj_id })
-
-    client.close()
-
-    d3_json = gnet['viz']
-    viz_name = gnet['name']
-
-    return render_template('gen3va.html', viz_network=d3_json, viz_name=viz_name)
+    net = get_network_from_mongo(user_objid)
+    return render_page(net, 'gen3va.html')
 
   @viz_page.route("/clustergrammer/harmonizome/<user_objid>")
   @viz_page.route("/clustergrammer/harmonizome/<user_objid>/<slug>")
   def viz_harmonizome(user_objid, slug=None):
-
-    client = MongoClient(mongo_address)
-    db = client.clustergrammer
-
-    try: 
-      obj_id = ObjectId(user_objid)
-    except:
-      error_desc = 'Invalid visualization Id.'
-      return redirect('/clustergrammer/error/'+error_desc)
-
-    gnet = db.networks.find_one({'_id': obj_id })
-
-    client.close()
-
-    d3_json = gnet['viz']
-    viz_name = gnet['name']
-
-    return render_template('harmonizome.html', viz_network=d3_json, viz_name=viz_name)    
+    net = get_network_from_mongo(user_objid)
+    return render_page(net, 'harmonizome.html')
 
   app.register_blueprint(viz_page)
