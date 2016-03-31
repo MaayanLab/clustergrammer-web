2e6be9d51204ce413caabd7820c789ab3f221557
cornhundred <doctorcorn2002@gmail.com>
2016-03-31 16:21:44 -0400

    unstable, defined viz_functions module that is used within viz_pages

--- clustergrammer/viz_pages/__init__.py
+++ clustergrammer/viz_pages/__init__.py
@@ -1,33 +1,5 @@
 from flask import Blueprint, render_template, redirect
-from pymongo import MongoClient
-from bson.objectid import ObjectId
-from copy import deepcopy
-mongo_address = '146.203.54.165'
-
-def get_network_from_mongo(user_objid):
-  client = MongoClient(mongo_address)
-  db = client.clustergrammer
-
-  try: 
-    obj_id = ObjectId(user_objid)
-  except:
-    obj_id = 'error'
-
-  if obj_id != 'error':
-    net = db.networks.find_one({'_id': obj_id })
-  else:
-    net = 'error'
-
-  client.close()
-
-  return net
-
-def render_page(net, page_route):
-  if net != 'error':
-    return render_template(page_route, viz_network=net['viz'], viz_name=net['name'])
-  else:
-    error_desc = 'Invalid visualization Id.'
-    return redirect('/clustergrammer/error/'+error_desc)  
+import viz_functions as viz_fun
 
 def add_routes(app=None):
 
@@ -37,25 +9,25 @@ def add_routes(app=None):
   @viz_page.route("/clustergrammer/viz/<user_objid>")
   @viz_page.route("/clustergrammer/viz/<user_objid>/<slug>")
   def viz(user_objid, slug=None):
-    net = get_network_from_mongo(user_objid)
-    return render_page(net, 'viz.html')
+    net = viz_fun.get_network_from_mongo(user_objid)
+    return viz_fun.render_page(net, 'viz.html')
 
   @viz_page.route("/clustergrammer/Enrichr/<user_objid>")
   @viz_page.route("/clustergrammer/Enrichr/<user_objid>/<slug>")
   def viz_Enrichr(user_objid, slug=None):
-    net = get_network_from_mongo(user_objid)
-    return render_page(net, 'Enrichr.html')
+    net = viz_fun.get_network_from_mongo(user_objid)
+    return viz_fun.render_page(net, 'Enrichr.html')
 
   @viz_page.route("/clustergrammer/gen3va/<user_objid>")
   @viz_page.route("/clustergrammer/gen3va/<user_objid>/<slug>")
   def viz_gen3va(user_objid, slug=None):
-    net = get_network_from_mongo(user_objid)
-    return render_page(net, 'gen3va.html')
+    net = viz_fun.get_network_from_mongo(user_objid)
+    return viz_fun.render_page(net, 'gen3va.html')
 
   @viz_page.route("/clustergrammer/harmonizome/<user_objid>")
   @viz_page.route("/clustergrammer/harmonizome/<user_objid>/<slug>")
   def viz_harmonizome(user_objid, slug=None):
-    net = get_network_from_mongo(user_objid)
-    return render_page(net, 'harmonizome.html')
+    net = viz_fun.get_network_from_mongo(user_objid)
+    return viz_fun.render_page(net, 'harmonizome.html')
 
   app.register_blueprint(viz_page)
---
+++ clustergrammer/viz_pages/viz_functions.py
@@ -0,0 +1,30 @@
+from flask import render_template, redirect
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+mongo_address = '146.203.54.165'
+
+def get_network_from_mongo(user_objid):
+  client = MongoClient(mongo_address)
+  db = client.clustergrammer
+
+  try: 
+    obj_id = ObjectId(user_objid)
+  except:
+    obj_id = 'error'
+
+  if obj_id != 'error':
+    net = db.networks.find_one({'_id': obj_id })
+  else:
+    net = 'error'
+
+  client.close()
+
+  return net
+
+def render_page(net, page_route):
+  if net != 'error':
+    return render_template(page_route, viz_network=net['viz'], viz_name=net['name'])
+  else:
+    error_desc = 'Invalid visualization Id.'
+    return redirect('/clustergrammer/error/'+error_desc)  
