98e576eedd49d4f8701ad355b3a50fcd519c9e87
cornhundred <doctorcorn2002@gmail.com>
2016-04-04 17:30:52 -0400

    using clustergrammer_old.py for main upload behavior, so that I can upgrade gen3va functionality faster if necessary

--- clustergrammer/load_tsv_file.py
+++ clustergrammer/load_tsv_file.py
@@ -4,53 +4,30 @@ def main( buff, inst_filename, mongo_address, viz_id):
   from bson.objectid import ObjectId
   from pymongo import MongoClient
   from flask import request
-  from clustergrammer import Network
+  from clustergrammer_old import Network
   import StringIO
 
-  ##############################
-  # set up database connection 
-  ##############################
-  # set up connection 
   client = MongoClient(mongo_address)
   db = client.clustergrammer
 
-  # get placeholder viz data 
   viz_id = ObjectId(viz_id)
   found_viz = db.networks.find_one({'_id':viz_id})
 
   try:
-    ########################
-    # load and cluster 
-    ########################
 
-    # initiate class network 
     net = Network()
-    # net.load_lines_from_tsv_to_net(file_lines)  
     net.pandas_load_tsv_to_net(buff)
 
-    # swap nans for zero 
     net.swap_nan_for_zero()
-
-    # deprecated clustering module
-    ####################################
-
-    # # fast mult views takes care of pre-filtering
-    # net.fast_mult_views()
-
-    ####################################
 
     net.make_filtered_views(dist_type='cosine', dendro=True, \
       views=['filter_row_sum'], linkage_type='average')
-
-    ###############################
-    # save to database 
-    ###############################
 
     export_dat = {}
     export_dat['name'] = inst_filename
     export_dat['dat'] = net.export_net_json('dat')
     export_dat['source'] = 'user_upload'
-    # save dat to separate document 
+
     dat_id = db.network_data.insert(export_dat)
 
     update_viz = net.viz 
@@ -63,16 +40,11 @@ def main( buff, inst_filename, mongo_address, viz_id):
     update_viz = 'error'
     update_dat = 'error'
 
-  # update found_viz 
   found_viz['viz'] = update_viz
   found_viz['dat'] = update_dat
 
-  # update found_viz in database 
   db.networks.update_one( {'_id':viz_id}, {'$set': found_viz} )
 
-  ############################
-  # end database connection 
-  ############################
   client.close()
 
 
