6429382e80ca3b9ac3f145c5016cb809fab2ca22
cornhundred <doctorcorn2002@gmail.com>
2016-04-04 15:50:03 -0400

    removed unnecessary module from run_enrich_background

--- clustergrammer/enrichr_clust/__init__.py
+++ clustergrammer/enrichr_clust/__init__.py
@@ -15,8 +15,9 @@ def add_routes(app=None, mongo_address=None):
 
   print('\n\n\nEnrichr_cluster\n\n\n')
 
-  enrichr_clust = Blueprint('enrichr_clust', __name__, static_url_path='/enrichr_clust/static',
-    static_folder='./static', template_folder='./templates')
+  enrichr_clust = Blueprint('enrichr_clust', __name__, 
+    static_url_path='/enrichr_clust/static', static_folder='./static', 
+    template_folder='./templates')
 
   @enrichr_clust.route("/clustergrammer/Enrichr_clustergram", methods=['POST','GET'])
   @cross_origin()
@@ -31,70 +32,48 @@ def add_routes(app=None, mongo_address=None):
 
     try:  
 
-      # submit placeholder to mongo 
-      ################################
-
-      # set up database connection 
       client = MongoClient(mongo_address)
       db = client.clustergrammer
 
-      # generate placeholder json - does not contain viz json 
       export_viz = {}
       export_viz['name'] = str(enr_json['userListId']) + '_' + enr_json['gmt']
       export_viz['viz'] = 'processing'
       export_viz['dat'] = 'processing'
       export_viz['source'] = 'Enrichr_clustergram'
 
-      # this is the id that will be used to view the visualization 
       viz_id = db.networks.insert( export_viz )
       viz_id = str(viz_id)
 
-      # close database connection 
       client.close()
       
-      # initialize thread
-      ######################
-      print('initializing thread')
       sub_function = enr_sub.Enrichr_cluster
       arg_list = [mongo_address, viz_id, enr_json['enr_list']]
       thread = threading.Thread(target=sub_function, args=arg_list)
       thread.setDaemon(True)
 
-      # run subprocess 
-      ####################
-      print('running subprocess and pass in viz_id ')
       thread.start()
 
       viz_url = 'http://amp.pharm.mssm.edu/clustergrammer/Enrichr_new/'
       qs = '?preview=false&col_order=rank&row_order=clust&N_row_sum=20'
 
-      # check if subprocess is finished 
-      ###################################
       max_wait_time = 30
-      print('check if subprocess is done')
       for wait_time in range(max_wait_time):
 
         # wait one second 
         time.sleep(1)
 
-        print('enrichr_clustergram wait_time: '+str(wait_time)+' '+str(thread.isAlive()))
-
         if thread.isAlive() == False:
 
-          print('\n\nenrichr_clustergram thread is dead\n----------\n')
-          
           return flask.jsonify({'link': viz_url+viz_id+qs})
 
       # return link after max time has elapsed 
       return flask.jsonify({'link': viz_url+viz_id+qs})
 
     except:
-      print('error making enrichr clustergram')
       error_desc = 'Error in processing Enrichr clustergram.'
 
       return flask.jsonify({
         'link': 'error'
       })     
-
 
   app.register_blueprint(enrichr_clust)
