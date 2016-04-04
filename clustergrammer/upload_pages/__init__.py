c121bbbddc34e1ae12d239e186cbe460adf32fd8
cornhundred <doctorcorn2002@gmail.com>
2016-04-04 17:16:16 -0400

    moved vector_upload endpoint behavior to its own function

--- clustergrammer/upload_pages/__init__.py
+++ clustergrammer/upload_pages/__init__.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template
 from flask.ext.cors import cross_origin
-from pymongo import MongoClient
 
 import Enrichr_clustergram_endpoint as enr_clust_endpoint
+import vector_upload_function as vector_upload_fun
 
 def add_routes(app=None, mongo_address=None):
 
@@ -19,95 +19,7 @@ def add_routes(app=None, mongo_address=None):
   @upload_pages.route('/clustergrammer/vector_upload/', methods=['POST'])
   @cross_origin()
   def proc_vector_upload():
-    import requests 
-    import flask
-    import json 
-    from clustergrammer import Network
-    import threading 
-    import time 
-    import run_g2e_background
 
-    try:
-      vector_post = json.loads(request.data)
-      inst_status = 'processing'
-    except:
-      inst_status = 'error'
-
-    client = MongoClient(mongo_address)
-    db = client.clustergrammer
-
-    export_viz = {}
-    if 'title' in vector_post:
-      if len(vector_post['title']) > 0:
-        export_viz['name'] = vector_post['title']
-      else:
-        export_viz['name'] = 'vector_post'
-    else:
-      export_viz['name'] = 'vector_post'
-      
-    export_viz['viz'] = inst_status
-    export_viz['dat'] = inst_status
-    export_viz['source'] = 'vector_post'
-
-    try: 
-      post_id = db.network_data.insert( vector_post )
-    except:
-      post_id = 'vector_post_too_large'
-
-    export_viz['post'] = post_id
-
-    viz_id = db.networks.insert( export_viz ) 
-    viz_id = str(viz_id)
-
-    client.close()
-
-    if inst_status == 'processing':
-
-      sub_function = run_g2e_background.main
-      arg_list = [ mongo_address, viz_id, vector_post ]
-      thread = threading.Thread(target=sub_function, args=arg_list)
-      thread.setDaemon(True)
-
-      thread.start()
-
-      if export_viz['name'] == 'gen3va':
-        viz_url = 'http://amp.pharm.mssm.edu/clustergrammer/gen3va/'
-        inst_name = ''
-      elif export_viz['name'] == 'harmonizome':
-        viz_url = 'http://amp.pharm.mssm.edu/clustergrammer/harmonizome/'
-        inst_name = ''      
-      else:
-        viz_url = 'http://amp.pharm.mssm.edu/clustergrammer/viz/'
-        inst_name = '/'+export_viz['name']
-
-      max_wait_time = 30
-      for wait_time in range(max_wait_time):
-
-        # wait one second 
-        time.sleep(1)
-
-        print('\twaiting '+str(wait_time)+' is alive '+str(thread.isAlive()))
-
-        if thread.isAlive() == False:
-
-          print('thread is finished')
-          
-          return flask.jsonify({
-            'link': viz_url+viz_id+inst_name,
-            'id':viz_id
-            })
-
-      return flask.jsonify({
-        'link': viz_url+viz_id+inst_name,
-        'id':viz_id
-        })
-
-    else:   
-
-      error_desc = 'Error in processing Enrichr enrichment vectors.'
-
-      return flask.jsonify({
-        'link': 'http://amp.pharm.mssm.edu/clustergrammer/error/'+error_desc
-      })     
+    return vector_upload_fun.main(mongo_address)
 
   app.register_blueprint(upload_pages)
