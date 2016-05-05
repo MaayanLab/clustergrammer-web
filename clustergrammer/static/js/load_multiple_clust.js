b0ae7cc8f7e0d50dee8c7f12bbcde15821b49089
cornhundred <doctorcorn2002@gmail.com>
2016-05-05 18:49:02 -0400

    viz_sim_mats test endpoint is progressing, single json working in container, working on calculating sim mats and loading them all simultaneously

---
+++ clustergrammer/static/js/load_multiple_clust.js
@@ -0,0 +1,141 @@
+function load_viz_multiple_clust(network_data){
+
+  var tmp_num;
+  var cat_colors;
+
+  make_clust(network_data, make_sim_mats)
+  console.log('load_viz_multiple_clust')
+
+  // tmo
+  unblock();
+}
+
+// make wait sign 
+$.blockUI({ css: { 
+    border: 'none', 
+    padding: '15px', 
+    backgroundColor: '#000', 
+    '-webkit-border-radius': '10px', 
+    '-moz-border-radius': '10px', 
+    opacity: .8, 
+    color: '#fff' 
+} });
+
+d3.select('.blockMsg').select('h1').text('Please wait...');
+
+var viz_size = {'width':1140, 'height':750};
+
+// define arguments object
+var default_args = {
+  'show_tile_tooltips':true,
+  'about':'Zoom, scroll, and click buttons to interact with the clustergram.',
+  'row_search_placeholder':'Gene'
+};
+
+$(document).ready(function(){
+    $(this).scrollTop(0);
+});
+
+
+resize_container();
+
+window.onscroll = function() {
+
+  var show_col_sim = 200;
+  var show_row_sim = 1200;
+  var hide_clust = 900;
+  var hide_col_sim = 1800;
+  var inst_scroll = $(document).scrollTop();
+
+  // // load col sim mat 
+  // if (inst_scroll > show_col_sim){
+  //   if (d3.select('#container-id-2 .viz_svg').empty()){
+  //     make_sim_mats('col', cat_colors)
+  //   }
+  // }
+
+  // // load row sim mat 
+  // if (inst_scroll > show_row_sim){
+  //   if (d3.select('#container-id-3 .viz_svg').empty()){
+  //     make_sim_mats('row', cat_colors)
+  //   }
+  // }
+
+  // hide clust 
+  if (inst_scroll > hide_clust){
+    d3.select('#container-id-1 .viz_svg')
+      .style('display', 'none');
+  } else {
+    d3.select('#container-id-1 .viz_svg')
+      .style('display', 'block');
+  }
+
+  // hide col sim mat
+  if (inst_scroll > hide_col_sim || inst_scroll < show_col_sim){
+    d3.select('#container-id-2 .viz_svg')
+      .style('display', 'none');
+  } else {
+    d3.select('#container-id-2 .viz_svg')
+      .style('display', 'block');
+  }
+
+}
+
+function make_clust(network_data, make_sim_mats){
+
+  var args = $.extend(true, {}, default_args);
+  args.root = '#container-id-1';
+  args.network_data = network_data;
+
+  cgm = Clustergrammer(args);
+  d3.select(cgm.params.root+' .wait_message').remove();
+  cat_colors = cgm.params.cat_colors;
+
+  // make_sim_mats('col', cat_colors, unblock);
+  // make_sim_mats('row', cat_colors, unblock);
+
+}
+
+function make_sim_mats(inst_rc, cat_colors, unblock){
+
+  clust_name = 'mult_view_sim_'+inst_rc+'.json'
+  d3.json('json/'+clust_name, function(network_data){
+
+    var args = $.extend(true, {}, default_args);
+    args.cat_colors = {};
+    if (inst_rc === 'col'){
+      tmp_num = 2;
+      args.cat_colors.row = cat_colors.col;
+      args.cat_colors.col = cat_colors.col;
+    } else if (inst_rc === 'row'){
+      tmp_num = 3;
+      args.cat_colors.row = cat_colors.row;
+      args.cat_colors.col = cat_colors.row;
+    }
+
+    args.root = '#container-id-'+tmp_num;
+
+    args.network_data = network_data;
+    cgm = Clustergrammer(args);
+    d3.select(cgm.params.root+' .wait_message').remove();
+    unblock();
+  });
+
+}
+
+function unblock(){
+  $.unblockUI();
+}
+
+function resize_container(){
+
+  var screen_width = viz_size.width;
+  var screen_height = viz_size.height;
+
+  d3.selectAll('.clustergrammer_container')
+    .style('width', screen_width+'px')
+    .style('height', screen_height+'px');
+}
+
+
+
