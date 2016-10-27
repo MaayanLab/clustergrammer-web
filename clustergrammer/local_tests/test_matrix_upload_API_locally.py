f7eb733d326dd6b1c5a377c4542cb355ff910ec4
Nick Fernandez <nickfloresfernandez@gmail.com>
2016-10-27 16:27:11 -0400

    working in local container, deployint 10-27-2016 425pm

--- clustergrammer/local_tests/test_matrix_upload_API_locally.py
+++ clustergrammer/local_tests/test_matrix_upload_API_locally.py
@@ -1,7 +1,8 @@
 import requests
 
 filename = 'example_matrix.txt'
-upload_url = 'http://localhost:9000/clustergrammer/matrix_upload/'
+# upload_url = 'http://localhost:9000/clustergrammer/matrix_upload/'
+upload_url = 'http://0.0.0.0:8087/clustergrammer/matrix_upload/'
 # upload_url = 'http://amp.pharm.mssm.edu/clustergrammer/matrix_upload/'
 
 r = requests.post(upload_url, files={'file': open(filename, 'rb')})
