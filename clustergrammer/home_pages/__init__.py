59718ba8b1ee4662ab40f59f72f67d441e0f8a6e
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-01-12 10:41:58 -0500

    removed unused upload_sim_mats.html template

--- clustergrammer/home_pages/__init__.py
+++ clustergrammer/home_pages/__init__.py
@@ -1,22 +1,22 @@
 from flask import Blueprint, render_template
 
 def add_routes(app=None):
-  home_pages = Blueprint('home_pages', __name__, static_url_path='/home_pages/static', 
+  home_pages = Blueprint('home_pages', __name__, static_url_path='/home_pages/static',
     static_folder='./static', template_folder='./templates')
 
   @home_pages.route("/clustergrammer/")
   @home_pages.route("/Clustergrammer/")
   @home_pages.route("/CLUSTERGRAMMER/")
   def index():
-    return render_template('index.html')  
+    return render_template('index.html')
 
-  @home_pages.route('/clustergrammer/upload_sim_mats/')
-  def upload_sim_mats():
-    return render_template('upload_sim_mats.html')
+  # @home_pages.route('/clustergrammer/upload_sim_mats/')
+  # def upload_sim_mats():
+  #   return render_template('upload_sim_mats.html')
 
   @home_pages.route("/clustergrammer/help")
   def help():
-    return render_template('help.html')      
+    return render_template('help.html')
 
   @home_pages.route("/clustergrammer/CCLE/")
   @home_pages.route("/Clustergrammer/CCLE/")
