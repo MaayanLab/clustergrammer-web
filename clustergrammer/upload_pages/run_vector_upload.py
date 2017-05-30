495e299ffa59a9fd72d81bcda73e1fbb6b0989fd
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-05-30 16:36:34 -0400

    using latest py 1.13.3 5-30-2017, working on fizing hzome_gram on front-end (using old version for vector post upload until I can check for G2E)

--- clustergrammer/upload_pages/run_vector_upload.py
+++ clustergrammer/upload_pages/run_vector_upload.py
@@ -18,7 +18,6 @@ def clust_vect(db, viz_doc, vector_post):
 
   # from clustergrammer import Network
   from clustergrammer_py_v112_vect_post_fix import Network
-  # from clustergrammer_py_v1_13_2 import Network
 
   try:
     net = Network()
