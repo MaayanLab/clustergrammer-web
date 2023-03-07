67704357d8c43c7491e4ba6aed2ed60a084c1f2e
Daniel Clarke <u8sand@gmail.com>
2023-03-07 15:33:03 -0500

    Address "server reached MaxRequestWorkers setting"
    Fixes #16

--- boot.sh
+++ boot.sh
@@ -1,3 +1,20 @@
+cat > /etc/apache2/mods-available/mpm_prefork.conf << EOF
+# prefork MPM
+# StartServers: number of server processes to start
+# MinSpareServers: minimum number of server processes which are kept spare
+# MaxSpareServers: maximum number of server processes which are kept spare
+# MaxRequestWorkers: maximum number of server processes allowed to start
+# MaxConnectionsPerChild: maximum number of requests a server process serves
+
+<IfModule mpm_prefork_module>
+        StartServers                     10
+        MinSpareServers           10
+        MaxSpareServers          20
+        MaxRequestWorkers         1500
+        MaxConnectionsPerChild   10000
+</IfModule>
+EOF
+
 adduser --disabled-password --gecos '' r
 cd /app/
 mod_wsgi-express setup-server wsgi.py --port=80 --user r --group r --server-root=/etc/app --socket-timeout=600 --limit-request-body=524288000
